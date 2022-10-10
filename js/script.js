bd0c97da05caa22c29d973c64c86f9878939c0d3
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-10-10 15:03:58 +0200

    Basic exercise

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@
  ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
  Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 */
+
+// ELABORAZIONE
+for(let i = 1; i <= 100; i++){
+    
+    if( i % 3 === 0 && i % 5 === 0){
+        console.log(i, "FizzBuzz");
+    } else if( i % 3 === 0){
+        console.log(i, "Fizz");
+    } else if( i % 5 === 0){
+        console.log(i, "Buzz");
+    } else {
+        console.log(i);
+    }
+}
